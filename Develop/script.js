79df5239f71ee5854464098bcc8442e19a848dd8
Chris Messa <blindsun95@gmail.com>
2023-04-04 15:47:25 -0500

    Added current day to heading text using Day.JS

--- Develop/script.js
+++ Develop/script.js
@@ -14,6 +14,7 @@ $(function () {
   function? How can DOM traversal be used to get the "hour-x" id of the
   time-block containing the button that was clicked? How might the id be
   useful when saving the description in local storage? */
+currentDay.text(dayjs().format('dddd, MMMM D'))
 timeBlocks.each(function() {
   var timeBlock = $(this);
   
@@ -36,10 +37,4 @@ timeBlocks.each(function() {
   past, present, and future classes? How can Day.js be used to get the
   current hour in 24-hour time?
    */
-
-/*   TODO: Add code to get any user input that was saved in localStorage and set
-  the values of the corresponding textarea elements. HINT: How can the id
-  attribute of each time-block be used to do this?
-   */
-  // TODO: Add code to display the current date in the header of the page.
 });
