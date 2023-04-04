ce815eb08eb47b77b3812a9155f700c5ec4b1a52
Chris Messa <blindsun95@gmail.com>
2023-04-04 15:07:00 -0500

    chose selectors for savebutton click function

--- Develop/script.js
+++ Develop/script.js
@@ -2,15 +2,25 @@
 the code isn't run until the browser has finished rendering all the elements
 in the html. */
 var rootEl = $('#root');
+var saveButton = $('.saveBtn');
+var timeBlocks = $('.time-block');
+
 
 $(function () {
+
 /*   TODO: Add a listener for click events on the save button. This code should
   use the id in the containing time-block as a key to save the user input in
   local storage. HINT: What does `this` reference in the click listener
   function? How can DOM traversal be used to get the "hour-x" id of the
   time-block containing the button that was clicked? How might the id be
   useful when saving the description in local storage? */
-  
+
+saveButton.click(function() {
+  var blockID = $(this).parent().attr('id');
+  var textAreaContent = $(this).parent().find('textarea').val();
+  console.log(textAreaContent);
+})
+
 /*   TODO: Add code to apply the past, present, or future class to each time
   block by comparing the id to the current hour. HINTS: How can the id
   attribute of each time-block be used to conditionally add or remove the
@@ -24,3 +34,6 @@ $(function () {
    */
   // TODO: Add code to display the current date in the header of the page.
 });
+
+
+// $('div[attribute="value"]');
