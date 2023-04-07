181b91b7c5661cb980c8f4eb55f07bccf2fc556a
Chris Messa <blindsun95@gmail.com>
2023-04-06 23:59:42 -0500

    moved files up

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-var rootEl = $('#root');
 var saveButton = $('.saveBtn');
 var timeBlocks = $('.time-block');
 var currentDay = $('#currentDay');
-var currentHourMilitary = parseInt(dayjs().format('H'));
+var currentHour = parseInt(dayjs().format('H'));
 
 $(function () {
 
@@ -11,22 +10,25 @@ currentDay.text(dayjs().format('dddd, MMMM D'))
 timeBlocks.each(function() {
 
   timeBlock = $(this);
+  console.log(timeBlock);
 
   var blockID = timeBlock.attr('id');
   var textArea = timeBlock.find('textarea');
-
+  
   textArea.val(localStorage.getItem(blockID));
 
   saveButton.click(function() {
     var textAreaContent = textArea.val();
     localStorage.setItem(blockID, textAreaContent);
   })
-  var blockInt = parseInt(blockID.slice(5, blockID.length))
-  if (blockInt < currentHourMilitary) {
+
+  var blockHour = parseInt(blockID.slice(5, blockID.length))
+
+  if (blockHour < currentHour) {
     timeBlock.addClass('past');
-  } else if (blockInt === currentHourMilitary) {
+  } else if (blockHour === currentHour) {
     timeBlock.addClass('present');
-  } else if (blockInt > currentHourMilitary) {
+  } else if (blockHour > currentHour) {
     timeBlock.addClass('future');
   }
 })
